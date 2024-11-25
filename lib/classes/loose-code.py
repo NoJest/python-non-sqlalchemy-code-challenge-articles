81ad56af92bead78046709c1f0e2bc0d355d689e
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-11-25 11:17:10 -0500

    the test for author immutability is stupid, im running the AttributeError: can't set attribute, which is causing me to fail, but literally means the attribute is immutable

---
+++ lib/classes/loose-code.py
@@ -0,0 +1,24 @@
+ # @property
+    # def author (self):
+    #     return self._author
+    
+    # @author.setter
+    # def author (self, new_value):
+    #     if isinstance(new_value, Author):
+    #         self._author = new_value
+       
+    
+    # @property
+    # def magazine (self):
+    #     return self._magazine
+    
+    # @magazine.setter
+    # def magazine (self, new_value):
+    #     if isinstance(new_value, Magazine) or isinstance(new_value, str):
+    #         self._magazine = new_value
+    
+    
+      # @name.setter
+    # def name (self, author_name):
+    #     if isinstance(author_name, str) and len(author_name) > 0:
+    #         self._name = author_name
--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,92 @@
+
+
 class Article:
+    
+    all = []
+    
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        self.__class__.all.append(self)  
+    
+    @property
+    def title (self):
+        return self._title 
+    
+    @title.setter
+    def title (self, new_title):
+        if isinstance(new_title, str) and 5 <= len(new_title) <= 50:
+            self._title = new_title
+      
         
 class Author:
+    
+    all =[]
+    
     def __init__(self, name):
-        self.name = name
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        self.__class__.all.append(self)  
 
     def articles(self):
-        pass
+        return [art for art in Article.all if art.author == self]
 
     def magazines(self):
-        pass
+        return list ( set ( [art.magazine for art in Article.all if art.author == self]))
 
     def add_article(self, magazine, title):
-        pass
-
+        new_article = Article(self,magazine,title)
+        return new_article
+    
     def topic_areas(self):
-        pass
-
+        topics = list(set(mag.category for mag in self.magazines()))
+        return topics if topics else None
+    
+    @property
+    def name (self):
+        return self._name       
+     
+     
 class Magazine:
+    
+    all_mags = []
+    newname = 0
+    
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self.__class__.all_mags.append(self)
 
     def articles(self):
-        pass
+        return [art for art in Article.all if art.magazine == self]
 
     def contributors(self):
-        pass
+        return list ( set ( [art.author for art in Article.all if art.magazine == self]))
 
     def article_titles(self):
-        pass
-
+        titles = [art.title for art in Article.all if art.magazine == self]
+        return titles if titles else None
+        
     def contributing_authors(self):
-        pass
+        from collections import Counter
+        author_count = Counter(art.author for art in Article.all if art.magazine == self)
+        contributors = [author for author, count in author_count.items() if count >2]
+        return contributors if contributors else None
+    
+    @property
+    def name (self):
+        return self._name 
+    @name.setter
+    def name (self, mag_name):
+        if isinstance(mag_name, str) and 2 <= len(mag_name) <= 16:
+            self._name = mag_name 
+       
+    @property
+    def category (self):
+        return self._category 
+    @category.setter
+    def category (self, mag_category):
+        if isinstance(mag_category, str) and len(mag_category) > 0:
+            self._category = mag_category 
+       
